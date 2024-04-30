42b9ae187d8600bbea3cd8894d3117797a845e25
fjk-ken <fjkken@gmail.com>
2024-04-30 22:00:09 +0900

    リファクタリング 定義系をsetting.jsに移動

---
+++ data/setting.js
@@ -0,0 +1,4 @@
+var areaTitles = ["そう思う", "どちらでもない", "そう思わない"];
+
+var mindLabels = ["競争", "達成感","存在感", "思いやり","関連性","自信","未来の思想家","主催者","発見者","信頼性"];
+var chartColor = ["#ffcccc", "#ccffcc", "#ccccff", "#ffffcc", "#ffccff", "#ccffff", "#ffaacc", "#aaffcc", "#ccaaff", "#ff88aa"];
