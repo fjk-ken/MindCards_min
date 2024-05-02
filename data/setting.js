f45c69303621334e488f395c6d63627ec645f816
fjk-ken <fjkken@gmail.com>
2024-05-02 18:03:19 +0900

    グラフの変更

--- data/setting.js
+++ data/setting.js
@@ -1,4 +1,5 @@
 var areaTitles = ["そう思う", "どちらでもない", "そう思わない"];
 
-var mindLabels = ["競争", "達成感","存在感", "思いやり","関連性","自信","未来の思想家","主催者","発見者","信頼性"];
-var chartColor = ["#ffcccc", "#ccffcc", "#ccccff", "#ffffcc", "#ffccff", "#ccffff", "#ffaacc", "#aaffcc", "#ccaaff", "#ff88aa"];
+var mindLabels = ["何でも勝ちたい", "やるぞ!","仲良くしたい", "思いやり","目立ちたい","未来にワクワク","やればできる","なぜ？どうて？","まかせて!","計画したい"];
+//var chartColor = ["#ffcccc", "#ccffcc", "#ccccff", "#ffffcc", "#ffccff", "#ccffff", "#ffaacc", "#aaffcc", "#ccaaff", "#ff88aa"];
+var chartColor = ["red", "yellow", "green", "pink", "orange", "purple", "lightblue", "navy", "brown", "gray"];
