102b4648dd9bfd15b15e4e6e3dd3e8f7e7e16a25
fjk-ken <fjkken@gmail.com>
2024-05-08 22:56:58 +0900

    自力でグラフを実装した

--- data/setting.js
+++ data/setting.js
@@ -1,5 +1,20 @@
-var areaTitles = ["そう思う", "どちらでもない", "そう思わない"];
+const areaTitles = ["そう思う", "どちらでもない", "そう思わない"];
 
-var mindLabels = ["何でも勝ちたい", "やるぞ!","仲良くしたい", "思いやり","目立ちたい","未来にワクワク","やればできる","なぜ？どうして？","まかせて!","計画したい"];
+//const mindLabels = ["何でも勝ちたい", "やるぞ!","仲良くしたい", "思いやり","目立ちたい","未来にワクワク","やればできる","なぜ？どうして？","まかせて!","計画したい"];
 //var chartColor = ["#ffcccc", "#ccffcc", "#ccccff", "#ffffcc", "#ffccff", "#ccffff", "#ffaacc", "#aaffcc", "#ccaaff", "#ff88aa"];
-var chartColor = ["red", "yellow", "green", "pink", "orange", "purple", "lightblue", "navy", "brown", "gray"];
+//const chartColor = ["red", "yellow", "green", "pink", "orange", "purple", "lightblue", "navy", "brown", "gray"];
+
+const mindData = [
+    {id: "type1", text: "何でも勝ちたい", color: "red", charactor: "kyousou_red.png"},
+    {id: "type2", text: "やるぞ!", color: "yellow", charactor: "tassei_yellow.png"},
+    {id: "type3", text: "仲良くしたい", color: "green", charactor: "kanrensei_green.png"},
+    {id: "type4", text: "思いやり", color: "pink", charactor: "omoiyari_pink.png"},
+    {id: "type5", text: "目立ちたい", color: "orange", charactor: "omoiyari_pink.png"},
+    {id: "type6", text: "未来にワクワク", color: "purple", charactor: "sonzaikan_orange.png"},
+    {id: "type7", text: "やればできる", color: "lightblue", charactor: "sinrai_sky.png"},
+    {id: "type8", text: "なぜ？どうして？", color: "navy", charactor: "hakkensya_navy.png"},
+    {id: "type9", text: "まかせて", color: "brown", charactor: "sinraisei_brown.png"},
+    {id: "type10", text: "計画したい", color: "gray", charactor: "syusaisya_gray.png"},
+];
+
+const maxgraph = 15;
---
+++ js/graph.js
@@ -0,0 +1,102 @@
+var graphData = [];
+var graphwork = null;
+
+const createGraph = (table, datas) => {
+    graphData = $.extend([], datas);
+    $('body').append(`<div id="graphwork" style="width: 100%"></div>`);
+    graphwork = $("#graphwork");
+
+    for (var data of graphData) {
+
+        data.percentage = data.value / maxgraph * 100;
+      
+        data.view_width = 0;
+        data.view_value = 0;
+
+        table.css({'border-collapse': 'collapse', 'width': '90%'});
+      
+        table.append(`
+            <tr style="height: 3em">
+                <th class="dataname">${data.text}</th>
+                <td class="tabletop graphcell"></td>
+                <td class="graphcell"></td>
+                <td class="graphcell"></td>
+            </tr>`);
+        graphwork.append(`
+            <div class="graphmargin"> 
+                <div class="graphbg"> 
+                    <div class="graphbar" id="graph${data.id}" style="background: ${data.color}"><span class="graphvalue"> </span> 
+                    <img src="image/${data.charactor}" alt="image" class="dataimg" id="img${data.id}"> 
+                </div> 
+            </div>`);
+      }
+      
+}
+
+var starttime = 0;
+var graphValue = [];
+const graphview = (data) =>{
+    starttime = new Date();
+    graphValue = data;
+    $(".dataimg").css({"visibility": "hidden"});
+    window.requestAnimationFrame(graphrefresh);
+}
+
+const graphrefresh = timestamp => {
+
+    for (i = 0; i < graphData.length; i ++) {
+        var data = graphData[i];
+        data.percentage = graphValue[i] / maxgraph * 100;
+
+        if (data.view_width < data.percentage) {
+            data.view_width = data.view_width + 1.5;
+  
+            if (data.view_width > (data.percentage * 0.8)) {
+                data.view_width = data.view_width - 0.3;
+            }
+  
+            data.view_value = data.view_width * maxgraph / 100;
+  
+        }
+        else
+        {
+            data.view_width = data.percentage;
+  
+        }
+        $("#graph" + data.id).width(data.view_width + '%');
+        if(graphValue[i] > 0){
+            $("#graph" + data.id + " span").text(graphValue[i]);
+            $("#img" + data.id).css({'visibility': 'visible'});
+            $("#img" + data.id).offset({left: $("#graph" + data.id).offset().left + $("#graph" + data.id).width() +5, top: $("#img" + data.id).offset().top});
+        }
+    }
+  
+    if ((new Date()).getTime() <= starttime.getTime() + 3000) {
+        window.requestAnimationFrame(graphrefresh);
+    }
+  
+  }
+  $(window).on("load",() => {
+    var width = $("tr").first().width() - $("th").first().width();
+    graphwork.width(width);
+    for(var i = 0; i < $(".graphmargin").length; i ++){
+        $($(".graphmargin")[i]).offset($($(".tabletop")[i]).offset());
+    }
+
+  
+  });
+  
+  $(window).resize(() => {
+    var width = $("tr").first().width() - $("th").first().width();
+    graphwork.width(width);
+    for(var i = 0; i < $(".graphmargin").length; i ++){
+        $($(".graphmargin")[i]).offset($($(".tabletop")[i]).offset());
+        var img = $($(".graphmargin")[i]).find("img").first();
+        var graphbar =  $($(".graphmargin")[i]).find(".graphbar");
+        $(img).offset({left: $(graphbar).offset().left + $(graphbar).width() +5, top: $(img).offset().top});
+//        $("img").parent($($(".graphmargin")[i])).offset({left: $(".graphbar").parent($($(".graphmargin")[i])).offset().left + $(".graphbar").parent($($(".graphmargin")[i])).width() +5, top: $("img").parent($($(".graphmargin")[i])).offset().top});
+
+    }
+    
+  });
+  
